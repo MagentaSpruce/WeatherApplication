d1987a653e47ccce464eccd6869b199ddb3caa46
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-22 15:41:52 -0400

    Fixed all current and known bugs.

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,20 +1,40 @@
-export const setLocationObject = (locationObj, coordsObj) => { //cordsObj = what we received from gelocation of browser // locationObj is what we created with our CurrentLocation class
+const WEATHER_API_KEY = "f604fe7e9c832f51efa69c074078e6ee";
+
+export const setLocationObject = (locationObj, coordsObj) => {
+    //cordsObj = what we received from gelocation of browser // locationObj is what we created with our CurrentLocation class
     const { lat, lon, name, unit } = coordsObj;
     locationObj.setLat(lat);
     locationObj.setLon(lon);
     locationObj.setName(name);
     if (unit) {
-        locationObj.setUnit(unit);
+      locationObj.setUnit(unit);
     }
-}
+  };
 
-export const getHomeLocation = () => {
+  export const getHomeLocation = () => {
     return localStorage.getItem("defaultWeatherLocation");
-}
+  };
+
+  export const getCoordsFromApi = async (entryText, units) => {
+        //Looks for entries that start and end with numbers which we assume are zip codes
+     const regex = /^\d+$/g;
+    const flag = regex.test(entryText) ? "zip" : "q";
+    const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+     //spaces turned into %20, lat/long changed, ect. Encodes URL.
+    const encodedUrl = encodeURI(url);
+    try {
+      const dataStream = await fetch(encodedUrl);
+      const jsonData = await dataStream.json();
+      return jsonData;
+    } catch (err) {
+      console.error(err.stack);
+    }   
+  };
+
 
 //eliminates spaces in the search field before, between, and after words (1 space b/t words)
-export const cleanText = text => {
+export const cleanText = (text) => {
     const regex = / {2,}/g;
     const entryText = text.replaceAll(regex, " ").trim();
     return entryText;
-}
+  };
--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -1,44 +1,45 @@
 export const setPlaceholderText = () => {
-    const input = document.getElementById('searchBar__text');
-    window.innerWidth < 415 ? (input.placeholder = "City, State, Country") : (input.placeholder = 'City, State, Country, or Zip Code')
-}
+    const input = document.getElementById("searchBar__text");
+    window.innerWidth < 400
+      ? (input.placeholder = "City, State, Country")
+      : (input.placeholder = "City, State, Country, or Zip Code");
+  };
 
-export const addSpinner = (element) => {
+  export const addSpinner = (element) => {
     animateButton(element);
     setTimeout(animateButton, 1000, element);
-}
+  };
 
-const animateButton = element => {
+  const animateButton = (element) => {
     element.classList.toggle("none");
     element.nextElementSibling.classList.toggle("block");
     element.nextElementSibling.classList.toggle("none");
-}
+  };
 
-export const displayError = (headerMsg, srMsg) => {
+  export const displayError = (headerMsg, srMsg) => {
     updateWeatherLocationHeader(headerMsg);
     updateScreenReaderConfirmation(srMsg);
-}
-
-export const displayApiError = statusCode => {
+  };
+  
+  export const displayApiError = (statusCode) => {
     const properMsg = toProperCase(statusCode.message);
     updateWeatherLocationHeader(properMsg);
     updateScreenReaderConfirmation(`${properMsg}. Please try again.`);
-    console.log(error);
-}
+  };
 
-const toProperCase = (text) => {
+  const toProperCase = (text) => {
     const words = text.split(" ");
-    const properWords = words.map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
+    const properWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
     });
     return properWords.join(" ");
-}
+  };
 
 const updateWeatherLocationHeader = message => {
     const h1 = document.getElementById("currentForecast__location");
     h1.textContent = message;
 }
 
-export const updateScreenReaderConfirmation = message => {
+export const updateScreenReaderConfirmation = (message) => {
     document.getElementById("confirmation").textContent = message;
-}
+  };
