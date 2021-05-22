836edef086a0cf5ca7565dd8d087955e045fd69f
MagentaSpruce <magentaspruce@protonmail.com>
2021-05-22 10:00:30 -0400

    Finished instituting the geolocation button with all relevant functions.Have started on the home button functionality and functions currently. Doing one button at a time.

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -8,3 +8,17 @@ const animateButton = icon => {
     icon.nextElementSibling.classList.toggle('block');
     icon.nextElementSibling.classList.toggle('none');
 }
+
+export const displayError = (headerMsg, srMsg) => {
+    updateWeatherLocationHeader(headerMsg);
+    updateScreenReaderConfirmation(srMsg);
+}
+
+const updateWeatherLocationHeader = message => {
+    const h2 = document.getElementById("currentForecast__location");
+    h2.textContent = message;
+}
+
+const updateScreenReaderConfirmation = message => {
+    document.getElementById('confirmation').textContent = message;
+}
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,23 @@
 'use strict';
+import { setLocationObject, getHomeLocation } from './dataFunctions.js';
 import CurrentLocation from "./CurrentLocation.js";
-import { addSpinner } from '../js/domFunctions.js';
+import { addSpinner, displayError } from '../js/domFunctions.js';
 
-const currentLoc = new CurrentLocation();
+
+
+const currentLocation = new CurrentLocation();
 
 const initApp = () => {
     // add listeners
     const geoButton = document.getElementById('getLocation');
     geoButton.addEventListener('click', getGeoWeather);
+    const homeButton = document.getElementById("home");
+    homeButton.addEventListener('click', loadWeather);
 
     // set up
 
     // load weather
+    loadWeather();
 }
 
 document.addEventListener('DOMContentLoaded', initApp);
@@ -24,6 +30,35 @@ const getGeoWeather = e => {
             addSpinner(mapIcon);
         }
     } 
-    // if (!navigator.geolocation) geoError();
-    // navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    if (!navigator.geolocation) geoError();
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 }
+
+const geoError = (errObj) => {
+    const errMsg = errObj ? errObj : "Geolocation not supported";
+    displayError(errMsg, errMsg);
+}
+
+// if geolocation succeeds, a position is given.
+const geoSuccess = position => {
+    const myCoordsObj = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        name: `Lat:${position.coords.latitude} Long:${position.coords.longitude}`
+    };
+    // set location object
+    setLocationObject(currentLocation, myCoordsObj);
+    console.log(currentLocation);
+ 
+    // update data and display
+    updateDataAndDisplay(currentLocation);
+};
+
+const loadWeather = e => {
+    const savedLocation = getHomeLocation();
+}
+
+const updateDataAndDisplay = async locationObj => {
+    // const weatherJson = await getWeatherFromCoords(locationObj);
+    // if (weatherJson) updateDisplay(weatherJson, locationObj); 
+};
